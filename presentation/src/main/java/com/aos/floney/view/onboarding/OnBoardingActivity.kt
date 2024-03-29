8fd3eb9ae9052ee1bcce15cbf8a1b2840805c1bb
jayyaj12 <arbert12@naver.com>
2024-03-29 10:36:54 +0900

    [Feat] add OnboardingActivity

---
+++ presentation/src/main/java/com/aos/floney/view/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,13 @@
+package com.aos.floney.view.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aos.floney.R
+import com.aos.floney.base.BaseActivity
+import com.aos.floney.databinding.ActivityOnBoardingBinding
+
+class OnBoardingActivity : BaseActivity<ActivityOnBoardingBinding, OnBoardingViewModel>(R.layout.activity_on_boarding) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ presentation/src/main/java/com/aos/floney/view/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,9 @@
+package com.aos.floney.view.onboarding
+
+import com.aos.floney.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(): BaseViewModel() {
+}
