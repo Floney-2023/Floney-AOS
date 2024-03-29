659aed0c273c3f204a0d651589ebdfe491cda6be
jayyaj12 <arbert12@naver.com>
2024-03-29 09:44:27 +0900

    [Feat] add SplashActivity

--- presentation/src/main/java/com/aos/floney/view/SplashActivity.kt
+++ presentation/src/main/java/com/aos/floney/view/SplashActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aos.floney.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
