26d038b862f6a2db0f7afbe2544d4eacb3f06d57
jayyaj12 <arbert12@naver.com>
2024-03-30 10:37:04 +0900

    [Feat] 회원가입 이동 기능 개발
    로그인 페이지 -> 회원가입 페이지 이동 기능 개발

--- presentation/src/main/java/com/aos/floney/view/login/LoginActivity.kt
+++ presentation/src/main/java/com/aos/floney/view/login/LoginActivity.kt
@@ -1,14 +1,31 @@
 package com.aos.floney.view.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.aos.floney.R
 import com.aos.floney.base.BaseActivity
 import com.aos.floney.databinding.ActivityLoginBinding
+import com.aos.floney.view.signup.SignUpActivity
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setUpViewModelObserver()
+    }
+
+    private fun setUpViewModelObserver() {
+        lifecycleScope.launch {
+            viewModel.clickSignUp.observe(this@LoginActivity) {
+                Timber.e("it $it")
+                if(it) {
+                    startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
+                }
+            }
+        }
     }
 }
--- presentation/src/main/java/com/aos/floney/view/login/LoginViewModel.kt
+++ presentation/src/main/java/com/aos/floney/view/login/LoginViewModel.kt
@@ -1,8 +1,13 @@
 package com.aos.floney.view.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.aos.floney.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,6 +15,10 @@ class LoginViewModel @Inject constructor(): BaseViewModel() {
 
     var email = MutableLiveData<String>()
     var password = MutableLiveData<String>()
+
+    // 회원가입 클릭
+    private var _clickSignUp = MutableLiveData<Boolean>()
+    val clickSignUp: LiveData<Boolean> get() = _clickSignUp
 
     // 로그인하기 클릭
     fun onClickLogin() {
@@ -23,7 +32,9 @@ class LoginViewModel @Inject constructor(): BaseViewModel() {
 
     // 회원가입 클릭
     fun onClickSignUp() {
+        Timber.e("onClickSignUp")
 
+        _clickSignUp.value = true
     }
 
     // 카카오 로그인 클릭
