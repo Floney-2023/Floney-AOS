5d15b9fe6cf8c470d59e097270afbe6f30aea9ae
wnsgml517 <wnsgml517@naver.com>
2024-04-02 18:17:20 +0900

    [Feat/#1] add 가계부 추가 (최초) activity, viewmodel

---
+++ presentation/src/main/java/com/aos/floney/view/book/add/BookAddActivity.kt
@@ -0,0 +1,12 @@
+package com.aos.floney.view.book.add
+
+import android.os.Bundle
+import com.aos.floney.R
+import com.aos.floney.base.BaseActivity
+import com.aos.floney.databinding.ActivityBookAddBinding
+
+class BookAddActivity : BaseActivity<ActivityBookAddBinding, BookAddViewModel>(R.layout.activity_book_add) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
