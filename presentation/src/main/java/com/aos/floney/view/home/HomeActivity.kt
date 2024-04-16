9a103e128c80cb47026cfe711a0c7cb2e39c9b35
jayyaj12 <arbert12@naver.com>
2024-04-16 22:17:28 +0900

    [Feat/#10] bottomNavigationView 구현

--- presentation/src/main/java/com/aos/floney/view/home/HomeActivity.kt
+++ presentation/src/main/java/com/aos/floney/view/home/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.databinding.library.baseAdapters.BR
+import androidx.navigation.fragment.NavHostFragment
 import com.aos.floney.R
 import com.aos.floney.base.BaseActivity
 import com.aos.floney.databinding.ActivityHomeBinding
@@ -26,6 +27,7 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
 
         setUpUi()
         setUpViewModelObserver()
+        setUpBottomNavigation()
     }
 
     private fun setUpUi() {
@@ -69,25 +71,56 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(R.layout.a
     }
 
     // 일별내역 아이템 클릭
-    fun onClickDayItem(item: DayMoney) { startActivity(
-        Intent(
-            this@HomeActivity,
-            HistoryActivity::class.java
-        ).putExtra("dayItem", DayMoneyModifyItem(
-            id = item.id,
-            money = item.money,
-            description = item.description,
-            lineDate = viewModel.getClickDate(),
-            lineCategory = item.lineCategory,
-            lineSubCategory = item.lineSubCategory,
-            assetSubCategory = item.assetSubCategory,
-            exceptStatus = item.exceptStatus,
-            writerNickName = item.writerNickName
-        ))
-    )
+    fun onClickDayItem(item: DayMoney) {
+        startActivity(
+            Intent(
+                this@HomeActivity,
+                HistoryActivity::class.java
+            ).putExtra(
+                "dayItem", DayMoneyModifyItem(
+                    id = item.id,
+                    money = item.money,
+                    description = item.description,
+                    lineDate = viewModel.getClickDate(),
+                    lineCategory = item.lineCategory,
+                    lineSubCategory = item.lineSubCategory,
+                    assetSubCategory = item.assetSubCategory,
+                    exceptStatus = item.exceptStatus,
+                    writerNickName = item.writerNickName
+                )
+            )
+        )
     }
 
     override fun onItemClick(item: DayMoney) {
         onClickDayItem(item)
+    }
+
+    fun setUpBottomNavigation(){
+        // 가운데 메뉴(제보하기)에 대한 터치 이벤트를 막기 위한 로직
+        binding.bottomNavigationView.apply {
+            menu.getItem(2).isEnabled = false
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeFragment -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    false
+                }R.id.homeFragment -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    false
+                }R.id.homeFragment -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    false
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            false
+        }
+
     }
 }
