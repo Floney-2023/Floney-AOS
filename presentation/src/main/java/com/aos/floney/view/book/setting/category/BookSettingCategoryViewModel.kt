0a278b68802d72e47303290c5902e928509c3a7c
wnsgml517 <wnsgml517@naver.com>
2024-06-29 22:45:02 +0900

    [FIX/#42] 분류항목 수정 중 <Alert 잠깐!> 보이기

--- presentation/src/main/java/com/aos/floney/view/book/setting/category/BookSettingCategoryViewModel.kt
+++ presentation/src/main/java/com/aos/floney/view/book/setting/category/BookSettingCategoryViewModel.kt
@@ -79,7 +79,13 @@ class BookSettingCategoryViewModel @Inject constructor(
 
     fun onClickPreviousPage() {
         viewModelScope.launch {
-            _back.emit(true)
+            if (edit.value!!)
+            {
+                _back.emit(false)
+            }
+            else{
+                _back.emit(true)
+            }
         }
     }
     // 자산/분류 카테고리 항목 가져오기
